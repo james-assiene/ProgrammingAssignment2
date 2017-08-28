3d1afff57f274c6906981928661ccf30f0d33554
james-assiene <assiene.james@gmail.com>
2017-08-28 20:01:56 +0100

    Exercise completed

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function creates an object (like OOP object) that will contain the matrix inverse.
+## This matrix will be accessed through accessors and mutators
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setMatrixInverse <- function(matrixInverse) m <<- matrixInverse
+  getMatrixInverse <- function() m
+  list(set = set, get = get,
+       setMatrixInverse = setMatrixInverse,
+       getMatrixInverse = getMatrixInverse)
 }
 
 
 ## Write a short comment describing this function
+## This function returns the inverse of the matrix encapsulated by "makeCacheMatrix"
+## The inverse will be retrieved from the cache if possible, computed otherwise.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getMatrixInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setMatrixInverse(m)
+  m  
 }
